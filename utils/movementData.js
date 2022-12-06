fa083304c81a49ab5721aa3ab7d525e7a114e617
ethanpaulfoster <epf@stanford.edu>
2022-12-05 22:27:57 -0800

    Minor

--- utils/movementData.js
+++ utils/movementData.js
@@ -1,16 +1,16 @@
-let movementData = [{dateEntry: "11/4/2022", motionEntry: [{feelings: ["anxious"], name: "", note: ""}, {feelings: ["anxious"], name: "squats 1", note: "I love these squats, they make me excited for my workout!"}, {feelings: ["joyful", "surprised"], name: "squats 1", note: ""}]},
-{dateEntry: "11/5/2022", motionEntry: [{feelings: ["angry"], name: "", note: ""}, {feelings: ["angry"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious"], name: "squats 2", note: ""}]},
-{dateEntry: "11/9/2022", motionEntry: [{feelings: ["joyful"], name: "", note: ""}, {feelings: ["joyful"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["surprised"], name: "squats 2", note: ""}]},
-{dateEntry: "11/12/2022", motionEntry: [{feelings: ["joyful"], name: "", note: ""}, {feelings: ["sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["sad"], name: "squats 2", note: ""}]},
-{dateEntry: "11/15/2022", motionEntry: [{feelings: ["anxious"], name: "", note: ""}, {feelings: ["anxious"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious"], name: "squats 2", note: ""}]},
+let movementData = [{dateEntry: "11/4/2022", motionEntry: [{feelings: ["anxious"], name: "11:30 1", note: ""}, {feelings: ["anxious"], name: "squats 1", note: "I love these squats, they make me excited for my workout!"}, {feelings: ["joyful", "surprised"], name: "squats 1", note: ""}]},
+{dateEntry: "11/5/2022", motionEntry: [{feelings: ["angry"], name: "11:33 1", note: ""}, {feelings: ["angry"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious"], name: "squats 2", note: ""}]},
+{dateEntry: "11/9/2022", motionEntry: [{feelings: ["joyful"], name: "11:30 1", note: ""}, {feelings: ["joyful"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["surprised"], name: "squats 2", note: ""}]},
+{dateEntry: "11/12/2022", motionEntry: [{feelings: ["joyful"], name: "11:30 1", note: ""}, {feelings: ["sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["sad"], name: "squats 2", note: ""}]},
+{dateEntry: "11/15/2022", motionEntry: [{feelings: ["anxious"], name: "11:30 1", note: ""}, {feelings: ["anxious"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious"], name: "squats 2", note: ""}]},
 
-{dateEntry: "11/18/2022", motionEntry: [{feelings: ["angry"], name: "", note: ""}, {feelings: ["angry"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["sad"], name: "squats 2", note: ""}]},
-{dateEntry: "11/20/2022", motionEntry: [{feelings: ["joyful"], name: "", note: ""}, {feelings: ["anxious"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["joyful"], name: "squats 2", note: ""}]},
-{dateEntry: "11/21/2022", motionEntry: [{feelings: ["angry"], name: "", note: ""}, {feelings: ["sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["sad"], name: "squats 2", note: ""}]},
-{dateEntry: "11/25/2022", motionEntry: [{feelings: ["surprised"], name: "", note: ""}, {feelings: ["surprised"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["joyful"], name: "squats 2", note: ""}]},
+{dateEntry: "11/18/2022", motionEntry: [{feelings: ["angry"], name: "11:30 1", note: ""}, {feelings: ["angry"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["sad"], name: "squats 2", note: ""}]},
+{dateEntry: "11/20/2022", motionEntry: [{feelings: ["joyful"], name: "11:30 1", note: ""}, {feelings: ["anxious"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["joyful"], name: "squats 2", note: ""}]},
+{dateEntry: "11/21/2022", motionEntry: [{feelings: ["angry"], name: "11:30 1", note: ""}, {feelings: ["sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["sad"], name: "squats 2", note: ""}]},
+{dateEntry: "11/25/2022", motionEntry: [{feelings: ["surprised"], name: "11:30 1", note: ""}, {feelings: ["surprised"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["joyful"], name: "squats 2", note: ""}]},
 
-{dateEntry: "11/27/2022", motionEntry: [{feelings: ["joyful", "sad"], name: "", note: ""}, {feelings: ["angry", "sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious", "surprised"], name: "squats 2", note: ""}]},
-{dateEntry: "11/29/2022", motionEntry: [{feelings: ["angry", "sad"], name: "", note: ""}, {feelings: ["angry", "sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious", "surprised"], name: "squats 2", note: ""}]}
+{dateEntry: "11/27/2022", motionEntry: [{feelings: ["joyful", "sad"], name: "11:30 1", note: ""}, {feelings: ["angry", "sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious", "surprised"], name: "squats 2", note: ""}]},
+{dateEntry: "11/29/2022", motionEntry: [{feelings: ["angry", "sad"], name: "11:30 1", note: ""}, {feelings: ["angry", "sad"], name: "squats 1", note: "Feeling these squats! Blah blah blah blah blah blah"}, {feelings: ["anxious", "surprised"], name: "squats 2", note: ""}]}
 
 ]
 
