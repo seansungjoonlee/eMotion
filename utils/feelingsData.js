10b5457589e28ca30ed0a44b5b665d3e5dfd5700
devorahrena <devorahrsimon@gmail.com>
2023-01-27 14:33:59 -0800

    Add files via upload

---
+++ utils/feelingsData.js
@@ -0,0 +1,74 @@
+const feelingsData = [
+    {
+        name: 'anxious',
+        color: '#FFA216',
+    },
+    {
+        name: 'stressed',
+        color: '#D46600',
+    },
+    {
+        name: 'frustrated',
+        color: '#F97D75',
+    },
+    {
+        name: 'tears',
+        color: '#FAFF11',
+    },
+    {
+        name: 'happy',
+        color: '#6BFF11',
+    },
+    {
+        name: 'small',
+        color: '#1EDDE9',
+    },
+    {
+        name: 'content',
+        color: '#02DB18',
+    },
+    {
+        name: 'discouraged',
+        color: '#87E9D1',
+    },
+    {
+        name: 'eager',
+        color: '#BEB9FF',
+    },
+    {
+        name: 'angry',
+        color: '#FF378B',
+    },
+    {
+        name: 'unmotivated',
+        color: '#11FFC6',
+    },
+    {
+        name: 'powerful',
+        color: '#FAFF11',
+    },
+    {
+        name: 'peaceful',
+        color: '#B2FF66',
+    },
+    {
+        name: 'aggressive',
+        color: '#FF6D3F',
+    },
+    {
+        name: 'sad',
+        color: '#1EE9C4',
+    },
+    {
+        name: 'optimistic',
+        color: '#D7FF66',
+    },
+    {
+        name: 'tired',
+        color: '#11D4FF',
+    },
+
+
+]
+
+export default feelingsData;
